77e934abec5b0868d5726fe9f7f3ee175dde2af4
shreyaraj6001 <raj006shreya@gmail.com>
2026-06-01 22:17:22 +0530

    Implement method to remove duplicates from sorted array

---
+++ Week_1/Day_1/RemoveDuplicatesFormSortedArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+     if (nums== null || nums.length == 0) return 0; 
+     int write = 1;
+     for (int read = 1; read < nums.length; read++) {
+        if (nums[read] != nums[read - 1]) {
+            nums[write] = nums[read];
+            write++;
+        }
+    }
+    return write;
+    }
+}
